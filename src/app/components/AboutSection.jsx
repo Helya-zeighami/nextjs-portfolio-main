dbd1d0c19899193274affaebbc215a6ef5872aae
Helya Zeighami <heleia.zeighami@gmail.com>
2024-02-02 00:53:51 +0330

    entire code

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,99 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>HTML, CSS, JS / ES6, Typescript</li>
+        <li>React.js</li>
+        <li>Nexjs</li>
+        <li>Bootstrap, Tailwind, Material UI, SCSS</li>
+        <li>Rest API, Rapid API, React Query</li>
+        <li>contextAPI, Redux</li>
+        <li>Git, Azure Devops</li>
+        <li>Adobe Xd, Figma</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Master in Computer Engineering</li>
+        <li>Bachelor in Computer Engineering</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Acronis Cloud Tech Foundation C21.03 | Sep 2021</li>
+        <li>Acronis Cloud Tech Associate Disaster Recovery | Sep 2021</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/about-image.png" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            I'm your coding buddy, proficient in JavaScript, HTML, CSS, and
+            ReactJS. Always hungry for knowledge, I'm on a mission to create
+            innovative and user-friendly web applications that make you go,
+            "Wow!"
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
